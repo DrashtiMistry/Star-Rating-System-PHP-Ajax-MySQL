a601e82fa28a224087907a6dd458850c143ac8d9
jency <jencymaheshwari2004@gmail.com>
2024-10-19 16:52:24 +0530

    Ratings Added

--- db/product.sql
+++ db/product.sql
@@ -1,45 +1,26 @@
--- CREATE TABLE `product` (
---   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
---   `name` varchar(256) NOT NULL,
---   `description` text NOT NULL,
---   `price` int(255) NOT NULL,
---   `image` varchar(255) NOT NULL,
---   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
---   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-
--- ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-
--- INSERT INTO `product` (`id`, `name`, `description`, `price`, `image`, `created_at`, `updated_at`) VALUES
--- (1, 'Yeh Jawani hai Deewani', '<ul style="margin:0px;padding:15px;">\r\n					<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	', 12, 'm1.png', '2019-01-19 14:13:04', '2019-01-19 14:13:04'),
--- (2, 'Dhadak', '<ul style="margin:0px;padding:15px;">\r\n				<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	', 21, 'm2.jpeg', '2019-01-19 14:13:04', '2019-01-19 14:13:04'),
--- (3, '3 Idiots', '<ul style="margin:0px;padding:15px;">\r\n				<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	', 18, 'm3.png', '2019-01-19 14:13:04', '2019-01-19 14:13:04'),
--- (4, 'Munjya', '<ul style="margin:0px;padding:15px;">\r\n				<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	', 20, 'm4.jpeg', '2019-01-19 14:13:04', '2019-01-19 14:13:04'),
--- (5, 'Chak de India!', '<ul style="margin:0px;padding:15px;">\r\n				<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	', 25, 'm5.jpeg', '2019-01-19 14:13:04', '2019-01-19 14:13:04');
-
-
-
 CREATE TABLE `product` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(256) NOT NULL,
   `description` text NOT NULL,
-  `image` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
 INSERT INTO `product` (`id`, `name`, `description`,  `image`) VALUES
-(1, 'Yeh Jawani hai Deewani', '<ul style="margin:0px;padding:15px;">\r\n					<li>Genre : Comedy, Drama</li>\r\n					<li>Stars : Ranbir Kapoor, Deepika Padukone</li>\r\n				<li>Director : Ayan Mukharji</li>\r\n			</ul>	', 'm1.png'),
+(1, 'Yeh Jawani hai Deewani', '<ul style="margin:0px;padding:15px;">\r\n					<li>Genre : Comedy, Drama</li>\r\n					<li>Stars : Ranbir Kapoor, Deepika Padukone</li>\r\n				<li>Director : Ayan Mukerji</li>\r\n			</ul>	', 'm1.png'),
 
-(2, 'Dhadak', '<ul style="margin:0px;padding:15px;">\r\n				<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	',  'm2.jpeg'),
+(2, 'Dhadak', '<ul style="margin:0px;padding:15px;">\r\n				<li>Genre : Romance, Drama</li>\r\n					<li>Stars : Jhanvi Kapoor, Ishan Khatter</li>\r\n				<li>Director : Shashank Khaitan</li>\r\n			</ul>	', 'm2.jpeg'),
 
-(3, '3 Idiots', '<ul style="margin:0px;padding:15px;">\r\n				<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	',  'm3.png'),
+(3, '3 Idiots', '<ul style="margin:0px;padding:15px;">\r\n				<li>Genre : Comedy, Drama</li>\r\n					<li>Stars : Kareena Kapoor, Amir Khan </li>\r\n				<li>Director : Rajkumar Hirani </li>\r\n			</ul>	',  'm3.png'),
 
-(4, 'Munjya', '<ul style="margin:0px;padding:15px;">\r\n				<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	',  'm4.jpeg'),
-(5, 'Chak de India!', '<ul style="margin:0px;padding:15px;">\r\n				<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	',  'm5.jpeg'),
-(6, 'Oh my God', '<ul style="margin:0px;padding:15px;">\r\n					<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	', 'm6.jpg'),
-(7, 'Kuch Kuch Hota hai', '<ul style="margin:0px;padding:15px;">\r\n				<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	',  'm7.jpg'),
-(8, 'Chup CHup Ke', '<ul style="margin:0px;padding:15px;">\r\n				<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	', 'm8.jpg'),
-(9, 'Om Shanti Om', '<ul style="margin:0px;padding:15px;">\r\n				<li>Built-in voice box </li>\r\n					<li>soft and great for cuddling</li>\r\n				<li>Special Design</li>\r\n			</ul>	', 'm9.jpeg');
+(4, 'Munjya', '<ul style="margin:0px;padding:15px;">\r\n				<li>Genre : Comedy, Horror </li>\r\n					<li>Stars : Abhay Verma , Sharvari </li>\r\n				<li>Director : Aditya Sarpotdar </li>\r\n			</ul>	',  'm4.jpeg'),
 
+(5, 'Chak de India', '<ul style="margin:0px;padding:15px;">\r\n				<li>Genre : Sports, Drama</li>\r\n					<li>Stars : Shah Rukh Khan,  Vidya Malvade </li>\r\n				<li>Director : Shimit Amin </li>\r\n			</ul>	',  'm5.jpeg'),
 
+(6, 'Oh my God', '<ul style="margin:0px;padding:15px;">\r\n					<li>Genre : Comedy, Drama</li>\r\n					<li>Stars : Paresh Raval , Akshay Kumar </li>\r\n				<li>Director : Umesh Shukla </li>\r\n			</ul>	', 'm6.jpg'),
 
+(7, 'Kuch Kuch Hota hai', '<ul style="margin:0px;padding:15px;">\r\n				<li>Genre : Romance, Drama</li>\r\n					<li>Stars : Shah Rukh Khan, Kajol , Rani Mukerji </li>\r\n				<li>Director : Karan Johar </li>\r\n			</ul>	',  'm7.jpg'),
+
+(8, 'Chup Chup Ke', '<ul style="margin:0px;padding:15px;">\r\n				<li>Genre : Comedy, Drama</li>\r\n					<li>Stars : Shahid Kapoor, Kareena Kapoor </li>\r\n				<li>Director : Priyadarshan </li>\r\n			</ul>	', 'm8.jpg'),
+
+(9, 'Om Shanti Om', '<ul style="margin:0px;padding:15px;">\r\n				<li>Genre : Romance, Drama</li>\r\n					<li>Stars : Shah Rukh Khan , Deepika Padukone</li>\r\n				<li>Director : Farah khan </li>\r\n			</ul>	', 'm9.jpeg');
--- db/product_rating.sql
+++ db/product_rating.sql
@@ -25,13 +25,25 @@ CREATE TABLE `product_rating` (
 --
 
 INSERT INTO `product_rating` (`id`, `product_id`, `user_id`, `rating_score`, `title`, `comment`, `reviewer`, `email`, `status`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, 4, 'its awesome', 'It''s awesome!!!','Joseph', 'joseph123@gmail.com',1, '2018-08-19 09:13:01', '2018-08-19 09:13:01'),
-(2, 2, 2, 5, 'Nice product', 'Really quality product!','Mary', 'mary123@gmail.com',1,  '2018-08-19 09:13:37', '2018-08-19 09:13:37'),
-(3, 1, 3, 3, 'I like it', 'its''s best but item.', 'John', 'john123@gmail.com', 1,'2018-08-19 09:14:19', '2018-08-19 09:14:19'),
-(4, 1, 4, 3, 'super awesome ', 'i think its supper products','lisa', 'lisa123@gmail.com', 1, '2018-08-19 09:18:00', '2018-08-19 09:18:00'),
-(5, 2, 5, 1, 'Good idea', 'Nice', 'Daniel', 'Daniel123@gmail.com',1, '2019-01-20 17:00:58',  '2019-01-20 17:00:58'),
-(6, 2, 5, 5, 'Nice product', 'this is nice!', 'Lee', 'Lee23@gmail.com',1, '2019-01-20 17:01:37', '2019-01-20 17:01:37'),
-(7, 1, 3, 4, 'really nice', 'Good!', 'Michael', 'Michael23@gmail.com', 1,'2019-01-20 21:06:48', '2019-01-20 21:06:48');
+
+(1, 1, 1, 4, 'A Journey of Friendship and Dreams
+', 'A fun and heartfelt journey of friendship, love, and self-discovery.','Drashti', 'drashti123@gmail.com',1, '2018-08-19 09:13:01', '2018-08-19 09:13:01'),
+
+(2, 2, 2, 5, 'A Love That Defied Boundaries', 'A passionate love story with a tragic twist, set against the backdrop of caste issues.','Jency', 'jency123@gmail.com',1,  '2018-08-19 09:13:37', '2018-08-19 09:13:37'),
+
+(3, 3, 3, 4, 'The Genius of Breaking Rules', 'A perfect blend of humor and life lessons, questioning the traditional education system.','Shreya', 'shreya123@gmail.com', 1,'2018-08-19 09:14:19', '2018-08-19 09:14:19'),
+
+(4, 4, 4, 5, 'Laughter with a Heart', 'Heartwarming comedy with a powerful message about compassion and human values.','Drashti', 'drashti123@gmail.com', 1, '2018-08-19 09:18:00', '2018-08-19 09:18:00'),
+
+(5, 5, 5, 5, 'The Spirit of Teamwork', 'An inspiring sports drama showcasing the power of teamwork and determination.', 'Jency', 'jency123@gmail.com',1, '2019-01-20 17:00:58',  '2019-01-20 17:00:58'),
+
+(6, 6, 5, 4, 'Faith vs. Rationality', 'A thought-provoking satire on religion and blind faith, raising important social questions.', 'Shreya', 'shreya123@gmail.com' ,1, '2019-01-20 17:01:37', '2019-01-20 17:01:37'),
+
+(7, 7, 3, 4, 'Love, Friendship, and Nostalgia', 'A timeless romantic drama with iconic love triangles and memorable performances.', 'Drashti', 'drashti123@gmail.com', 1,'2019-01-20 21:06:48', '2019-01-20 21:06:48'),
+
+(8, 8, 3, 4, 'Comedy of Errors', 'A light-hearted comedy of errors filled with funny moments and a heartwarming plot.', 'Jency', 'jency123@gmail.com', 1,'2019-01-20 21:06:48', '2019-01-20 21:06:48'),
+
+(9, 9, 3, 4, 'Bollywood Reimagined', 'A vibrant and entertaining mix of romance, reincarnation, and Bollywood nostalgia.','Shreya', 'shreya123@gmail.com', 1,'2019-01-20 21:06:48', '2019-01-20 21:06:48');
 
 
 
